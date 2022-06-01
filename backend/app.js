b461e6af35633a4b35b50ab131f2604558b7658f
vijay <pvijju198@gmail.com>
2022-06-01 15:09:19 +0530

    make it better

--- backend/app.js
+++ backend/app.js
@@ -331,6 +331,6 @@ app.get('/', function (req, res) {
     res.send('<h1>Welcome to the Pet Store</h1>');
 });
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
 });
